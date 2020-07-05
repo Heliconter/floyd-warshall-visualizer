88499605d4cc569f059b5aef93bc83b0cf5260fb
pjcwv <pajecawav@yahoo.com>
2020-07-05 23:27:44 +0500

    Add getters to ExecutorInterface

--- src/main/java/floydwarshall/executor/ExecutorInterface.java
+++ src/main/java/floydwarshall/executor/ExecutorInterface.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 public interface ExecutorInterface {
     void setGraph(int verticesAmount, ArrayList<Edge> edges);
     Integer getPathLength(PathEnds pathEnds);
+    int getK();
+    int getFrom();
+    int getTo();
     int getVerticesAmount();
     void step(int amount);
     void toEnd();
