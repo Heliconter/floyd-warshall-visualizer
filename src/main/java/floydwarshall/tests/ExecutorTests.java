5549c4aedbfb9eeee82b53381a3003d5403393b2
pjcwv <pajecawav@yahoo.com>
2020-07-07 20:01:29 +0500

    Attempt to add JUnit 5 to gradle

---
+++ src/main/java/floydwarshall/tests/ExecutorTests.java
@@ -0,0 +1,17 @@
+package floydwarshall.tests;
+
+import floydwarshall.executor.Executor;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class ExecutorTests {
+    @BeforeAll
+    void initExecutor() {
+        System.out.println("123");
+    }
+}
