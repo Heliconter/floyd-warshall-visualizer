491b4d94e2a9ee8605465c10c613b332ab30b761
AlexRyzhickov <makseljoinb@gmail.com>
2020-07-08 13:57:39 +0300

    delete only txt and add rewrite

--- src/main/java/floydwarshall/saveload/GraphWriter.java
+++ src/main/java/floydwarshall/saveload/GraphWriter.java
@@ -7,12 +7,12 @@ public class GraphWriter {
     public boolean createFileWithGraphData(String path, String data) throws Exception {
         boolean isFileCreated = false;
         File file = new File(path);
-        if (file.createNewFile()) {
+        //if (file.createNewFile()) {
             FileWriter writer = new FileWriter(file);
             writer.write(data);
             writer.close();
             isFileCreated = true;
-        }
+        //}
         return isFileCreated;
     }
 }
